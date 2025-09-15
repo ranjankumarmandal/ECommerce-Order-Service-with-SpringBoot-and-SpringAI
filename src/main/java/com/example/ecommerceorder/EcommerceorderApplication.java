6d98e1375251722a1fde34de9898c395d78ba6f9
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2025-09-15 19:39:06 +0530

    Spring Boot: ECommerce: Order Service: intial setup

---
+++ src/main/java/com/example/ecommerceorder/EcommerceorderApplication.java
@@ -0,0 +1,13 @@
+package com.example.ecommerceorder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceorderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceorderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ecommerceorder/EcommerceorderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ecommerceorder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceorderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
