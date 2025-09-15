65dfa73ec7b48507fe65d41a0db84b3a5a2a5dd2
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2025-09-15 21:38:15 +0530

    Spring Boot: ECommerce: Order Service: added controller layer changes

---
+++ src/main/java/com/example/ecommerceorder/controller/OrderController.java
@@ -0,0 +1,32 @@
+package com.example.ecommerceorder.controller;
+
+import com.example.ecommerceorder.entity.Order;
+import com.example.ecommerceorder.service.OrderService;
+import com.example.order.exception.ProductNotAvailableException;
+import io.swagger.v3.oas.annotations.Operation;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/orders")
+@RequiredArgsConstructor
+public class OrderController {
+
+    private final OrderService orderService;
+
+    @Operation(summary = "Place an order for a product by ID")
+    @PostMapping
+    public ResponseEntity<?> createOrder(@RequestParam Long productId) {
+        try {
+            Order order = orderService.placeOrder(productId);
+            return ResponseEntity.ok(order);
+        } catch (ProductNotAvailableException ex) {
+            return ResponseEntity.status(404).body(ex.getMessage());
+        } catch (Exception ex) {
+            return ResponseEntity.status(500).body("Internal Error: " + ex.getMessage());
+        }
+    }
+}
+
+
